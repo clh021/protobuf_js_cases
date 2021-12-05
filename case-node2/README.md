3f9de1958d876125f07834e50f2e772db31cd32f
chenlianghong <clh021@gmail.com>
2021-12-05 17:58:08 +0800

    add case-node2

---
+++ case-node2/README.md
@@ -0,0 +1,14 @@
+## Use
+
+```shell
+  npm install
+  node test.js
+```
+
+## Result
+
+```shell
+make run
+node test.js
+<Buffer 08 01 12 02 08 01>
+```
---
+++ case-node2/test.js
@@ -0,0 +1,23 @@
+
+const PROTO_PATH = `./window.proto`;
+
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+
+const packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  {
+    keepCase: true,
+  },
+);
+const lnks_proto = grpc.loadPackageDefinition(packageDefinition).lnks;
+const rpc = lnks_proto.Test.service.send
+
+// console.log("lnks_proto:", lnks_proto)
+// var winId1 = lnks_proto.WindowId;
+// winId1.setOk(true);
+// var winList = new lnks_proto.MainWindowList();
+
+// console.log(winId1);
+
+console.log(rpc.requestSerialize({ ok: true, windows: [{ chromium_id: 1 }] }));
